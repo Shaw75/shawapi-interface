36c7fd8007c492ccb7152a7c6030c1594a6a2d7e
shaw775 <admin@shaw33.cc>
2023-03-16 20:19:21 +0800

    Initial commit

---
+++ src/main/java/cc/shaw/shawapiinterface/ShawapiInterfaceApplication.java
@@ -0,0 +1,13 @@
+package cc.shaw.shawapiinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShawapiInterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShawapiInterfaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cc/shaw/shawapiinterface/ShawapiInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.shaw.shawapiinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShawapiInterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
